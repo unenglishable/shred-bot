1ccd91c32108aff248b3fe6be5cf2f28ac2a7db5
unenglishable <unenglishable@gmail.com>
2023-02-05 09:54:04 -0800

    feat(main): use example main

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import OS
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} ready to shred!')
+
+@client.event
+async def on_message(message):
+    # do nothing if message is from self
+    if message.author == client.user:
+        return
+    if message.content.startswith('$shred'):
+        await message.channel.send('shoots')
+
+client.run(os.getenv('TOKEN'))
